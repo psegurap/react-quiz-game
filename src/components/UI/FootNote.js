6c268a4197f592f4a9a856853a249127f8cbcc26
Pedro Segura <51924485+psegurap@users.noreply.github.com>
2026-01-10 23:13:53 -0500

    delete files in main

--- src/components/UI/FootNote.js
+++
@@ -1,7 +0,0 @@
-export default function FootNote(props) {
-    return (
-        <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:items-center md:justify-between dark:bg-gray-800 dark:border-gray-600">
-            {props.children}
-        </footer>
-    );
-}
